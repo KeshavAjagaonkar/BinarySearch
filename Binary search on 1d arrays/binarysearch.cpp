9edc6cb0c0833fc3dfc249262865411e9b1e74fd
Keshav_ajagaonkar <keshavvamanajagaonkar@gmail.com>
2025-07-15 12:16:45 +0530

    initial commit : binary search DSA problems

---
+++ Binary search on 1d arrays/binarysearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void print(vector<int> &arr){
+    for (int i = 0; i < arr.size();i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int binarySearch(vector<int>&arr,int k)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    while(low<=high){
+        int mid = (low + high) / 2;
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(mid>k){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int x;
+    vector<int> arr;
+    for (int i = 0; i < n;i++){
+        cin >> x;
+        arr.push_back(x);
+    }
+    print(arr);
+
+
+}
+
