cbe596df21f60573f73a1bc7e88dedcd85680885
Keshav_ajagaonkar <keshavvamanajagaonkar@gmail.com>
2025-07-18 10:22:25 +0530

    searchinrotatedarray.cpp is added

---
+++ Binary search on 1d arrays/searchinrotatedarray.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int search(vector<int> &arr, int key)
+{
+    // Code Here
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= key && key <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] <= key && key <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int x;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        arr.push_back(x);
+    }
+    print(arr);
+    int k = 3;
+
+    int index = search(arr, k);
+
+    cout << index;
+    return 0;
+}
